e9c0ae110ded4fd6737bb00809e0a7872465e947
Xu Feng Marcus Goh <s10187800@connect.np.edu.sg>
2019-07-21 18:18:40 +0800

    Populated listview

--- app/src/main/java/np/edu/sg/madassignment/MainActivity.java
+++ app/src/main/java/np/edu/sg/madassignment/MainActivity.java
@@ -6,11 +6,37 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity
 {
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView=(ListView)findViewById(R.id.listview);
+
+        ArrayList<String> arrayList=new ArrayList<>();
+
+        arrayList.add("Aerobic Exercise");
+        arrayList.add("Strength Training");
+        arrayList.add("Stretching");
+        arrayList.add("Balance Exercise");
+
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+
+
+    }
 
 }
