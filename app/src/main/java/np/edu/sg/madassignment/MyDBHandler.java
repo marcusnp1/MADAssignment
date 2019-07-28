de571cc8c359a9fa4649e25fc9cbaa6bb5d6541e
study <study@STUDYNOIRE>
2019-07-29 01:46:08 +0800

    Change Time counting

--- app/src/main/java/np/edu/sg/madassignment/MyDBHandler.java
+++ app/src/main/java/np/edu/sg/madassignment/MyDBHandler.java
@@ -17,6 +17,10 @@ public class MyDBHandler extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "Exercises.db";
     public static final String TABLE_COMMENTS = "comments";
+    public static final String TABLE_COMMENTS_2 = "comments2";
+    public static final String TABLE_COMMENTS_3 = "comments3";
+    public static final String TABLE_COMMENTS_4 = "comments4";
+
     public static final String COLUMN_ID = "UserID";
     public static final String COLUMN_NAME = "UserComments";
 
@@ -25,37 +29,90 @@ public class MyDBHandler extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, factory, DATABASE_VERSION);
     }
 
-    @Override
+    @Override//creates 4 tables on create to store data for each exercise
     public void onCreate(SQLiteDatabase db) {
+
         String CREATE_STUDENT_TABLE = "CREATE TABLE " +
                 TABLE_COMMENTS + "(" + COLUMN_ID + " ," + COLUMN_NAME
                 + " TEXT " + ")";
         db.execSQL(CREATE_STUDENT_TABLE);
 
+        String CREATE_STUDENT_TABLE_2 = "CREATE TABLE comments2 " +
+                "(" + COLUMN_ID + " ," + COLUMN_NAME
+                + " TEXT " + ")";
+        db.execSQL(CREATE_STUDENT_TABLE_2);
+        String CREATE_STUDENT_TABLE_3 = "CREATE TABLE comments3 " +
+                "(" + COLUMN_ID + " ," + COLUMN_NAME
+                + " TEXT " + ")";
+        db.execSQL(CREATE_STUDENT_TABLE_3);
+        String CREATE_STUDENT_TABLE_4 = "CREATE TABLE comments4 " +
+                "(" + COLUMN_ID + " ," + COLUMN_NAME
+                + " TEXT " + ")";
+        db.execSQL(CREATE_STUDENT_TABLE_4);
+
     }
 
-    @Override
+    @Override// Updates the database
     public void onUpgrade(SQLiteDatabase db, int oldVersion,
                           int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS_2);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS_3);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS_4);
         onCreate(db);
 
     }
-
-    public void addHandler(Comments comment) {
+    //Method to add comments into the database table to store.
+    public void addHandler(Comments comment, int type) {
         ContentValues values = new ContentValues();
+        String query;
+        if(type == 1)//checks for which table to use
+        {
+            query = "Select * FROM " + TABLE_COMMENTS;
+        }
+        else if(type == 2)
+        {
+            query = "Select * FROM " + TABLE_COMMENTS_2;
+        }
+        else if(type == 3)
+        {
+            query = "Select * FROM " + TABLE_COMMENTS_3;
+        }
+        else if(type == 4)
+        {
+            query = "Select * FROM " + TABLE_COMMENTS_4;
+        }
+        else{
+            query = "Select * FROM " + TABLE_COMMENTS;
+        }
         SQLiteDatabase db = this.getWritableDatabase();
-        String query = "Select * FROM " + TABLE_COMMENTS;
         Cursor cursor = db.rawQuery(query, null);
         cursor.moveToLast();
-        Comments commen = new Comments();
+        Comments commen = new Comments(); //create a new comment
         commen.setUserID(Integer.parseInt(cursor.getString(0)));
         cursor.close();
-        int id = commen.getUserID();
+        int id = commen.getUserID();//obtain the ID for the comment
+
         id += 1;
         values.put(COLUMN_ID, id);
         values.put(COLUMN_NAME, comment.getUserComments());
-        db.insert(TABLE_COMMENTS, null, values);
+        if(type == 1)//detemines which table to add the comment
+        {
+            db.insert(TABLE_COMMENTS, null, values);
+        }
+        else if(type == 2)
+        {
+            db.insert(TABLE_COMMENTS_2, null, values);
+        }
+        else if(type == 3)
+        {
+            db.insert(TABLE_COMMENTS_3, null, values);
+        }
+        else if(type == 4)
+        {
+            db.insert(TABLE_COMMENTS_4, null, values);
+        }
+
         db.close();
     }
 
@@ -76,10 +133,27 @@ public class MyDBHandler extends SQLiteOpenHelper {
         db.close();
         return comments;
     }
-
-    public ArrayList<String> loadHandler() {
+    //Method to load the comments from the database to the app.
+    public ArrayList<String> loadHandler(int type) {
         ArrayList<String> result = new ArrayList<>();
-        String query = "Select*FROM " + TABLE_COMMENTS ;
+        String query;
+        //detemines which table to load
+        if(type == 1){
+            query = "Select*FROM " + TABLE_COMMENTS ;
+        }
+        else if(type == 2) {
+            query = "Select*FROM " + TABLE_COMMENTS_2 ;
+        }
+        else if(type == 3){
+            query = "Select*FROM " + TABLE_COMMENTS_3 ;
+        }
+        else if(type == 4){
+            query = "Select*FROM " + TABLE_COMMENTS_4 ;
+        }
+        else{
+            query = "Select*FROM " + TABLE_COMMENTS ;
+        }
+
 
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor cursor = db.rawQuery(query, null);
--- app/src/main/java/np/edu/sg/madassignment/main2.java
+++ app/src/main/java/np/edu/sg/madassignment/main2.java
@@ -43,13 +43,9 @@ public class main2 extends AppCompatActivity
         commentbutton = findViewById(R.id.btnComment);
         commenttext = findViewById(R.id.txtComment);
         comments = findViewById(R.id.lv_comments);
-        ArrayList<String> List = new ArrayList<>(dbHandler.loadHandler());
-        List.add("Yes");
-        ArrayAdapter adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,List);
-        comments.setAdapter(adapter);
         Intent intent = getIntent();
         String grpName = ((Intent) intent).getStringExtra("ID");
-        //Populating individual pages with different routines
+        //Populating individual pages with different routines, also to show the correct comments for respective exercises.
         switch(grpName) {
             case "Aerobic Exercise":
                 imgexercise.setImageResource(R.drawable.march);
@@ -57,6 +53,9 @@ public class main2 extends AppCompatActivity
                 description.setText("Marching is a good exercise\nfor your heart and lungs\n" +
                         "Bend your elbows and\nswing your arms as you lift your knees.\n" +
                         "Your Knee should be lifted up to stomach level");
+                ArrayList<String> List1 = new ArrayList<>(dbHandler.loadHandler(1));
+                ArrayAdapter adapter1 = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,List1);
+                comments.setAdapter(adapter1);
                 break;
             case "Strength Training":
                 imgexercise.setImageResource(R.drawable.squat);
@@ -64,6 +63,9 @@ public class main2 extends AppCompatActivity
                 description.setText("Squating is a good exercise for your Muscles\n" +
                         "Bend your hips and knees.\nSwinging arms forwards helps in balance\n" +
                         "Your back should be kept straight");
+                ArrayList<String> List2 = new ArrayList<>(dbHandler.loadHandler(2));
+                ArrayAdapter adapter2 = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,List2);
+                comments.setAdapter(adapter2);
                 break;
             case "Stretching":
                 imgexercise.setImageResource(R.drawable.rotate);
@@ -71,6 +73,9 @@ public class main2 extends AppCompatActivity
                 description.setText("Single Knee Rotation is a good exercise for your flexibility\n" +
                         "Bend your knee to the opposite direction\nwhile putting your foot on the thigh.\n" +
                         "Pull your knee towards the\nopposite direction and hold for 10-30 seconds.");
+                ArrayList<String> List3 = new ArrayList<>(dbHandler.loadHandler(3));
+                ArrayAdapter adapter3 = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,List3);
+                comments.setAdapter(adapter3);
                 break;
             case "Balance Exercise":
                 imgexercise.setImageResource(R.drawable.kneelift);
@@ -79,9 +84,11 @@ public class main2 extends AppCompatActivity
                 description.setText("Standard Knee Lifting is a good exercise for your stability\n" +
                         "Lift your knee towards the ceiling\nas high as possible then lower it\nafter you feel the stretch\n" +
                         "Repeat this 3-5 times with both legs.");
+                ArrayList<String> List4 = new ArrayList<>(dbHandler.loadHandler(4));
+                ArrayAdapter adapter4 = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,List4);
+                comments.setAdapter(adapter4);
         }
     }
-
 
 
 
@@ -176,12 +183,38 @@ public class main2 extends AppCompatActivity
         int id = 0;
         String name = commenttext.getText().toString();
         Comments comment = new Comments(id, name);
-        dbHandler.addHandler(comment);
-        commenttext.setText("");
-        ArrayList<String> List = dbHandler.loadHandler();
-        ArrayAdapter adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,List);
-        comments.setAdapter(adapter);
-
+        Intent intent = getIntent();
+        String grpName = ((Intent) intent).getStringExtra("ID");
+        switch(grpName) {       //Checks for which exercise the user is in to add the comments into the corrrect table.
+            case "Aerobic Exercise":
+                dbHandler.addHandler(comment,1);
+                commenttext.setText("");
+                ArrayList<String> List1 = dbHandler.loadHandler(1);
+                ArrayAdapter adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, List1);
+                comments.setAdapter(adapter1);
+                break;
+            case "Strength Training":
+                dbHandler.addHandler(comment,2);
+                commenttext.setText("");
+                ArrayList<String> List2 = dbHandler.loadHandler(2);
+                ArrayAdapter adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, List2);
+                comments.setAdapter(adapter2);
+                break;
+            case "Stretching":
+                dbHandler.addHandler(comment,3);
+                commenttext.setText("");
+                ArrayList<String> List3 = dbHandler.loadHandler(3);
+                ArrayAdapter adapter3 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, List3);
+                comments.setAdapter(adapter3);
+                break;
+            case "Balance Exercise":
+                dbHandler.addHandler(comment,4);
+                commenttext.setText("");
+                ArrayList<String> List4 = dbHandler.loadHandler(4);
+                ArrayAdapter adapter4 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, List4);
+                comments.setAdapter(adapter4);
+                break;
+        }
     }
 
 }
